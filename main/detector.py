33e3e282288f1b48a70e642f5950eb57b64cc9d7
Francisco Henriques <franciscolealhenriques@usp.br>
2025-11-22 18:03:44 +0100

    Add flags for clustering and median filtering in detect_paired_regions

--- main/detector.py
+++ main/detector.py
@@ -5,6 +5,7 @@ from sklearn.cluster import DBSCAN
 from skimage.color import rgb2gray, hsv2rgb
 from main.patchmatch import PatchMatch
 from utils.image_tools import get_patch, diff
+from tqdm import trange
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -26,7 +27,14 @@ class CopyPasteDetector:
             elif self.img.shape[2] == 2:
                 self.img = self.img[..., 0]
 
-    def detect_paired_regions(self, cluster_eps=2.0, flat_threshold=0.0, min_cluster_size=200):
+    def detect_paired_regions(
+            self,
+            cluster_eps=2.0,
+            flat_threshold=0.0,
+            clustering=True,
+            min_cluster_size=50,
+            median_filtering=True
+    ):
         """
         Identify source region and its copies by clustering offset vectors
 
@@ -34,11 +42,12 @@ class CopyPasteDetector:
         """
         h, w = self.offsets.shape[:2]
 
-        # in noisy images, since the offsets can have slight differences,
-        # we uniformize then by applying a median filter
-        filt_col = median_filter(self.offsets[..., 0], size=self.patch_size)
-        filt_lin = median_filter(self.offsets[..., 1], size=self.patch_size)
-        self.offsets = np.stack([filt_col, filt_lin], axis=-1)
+        if median_filtering:
+            # in noisy images, since the offsets can have slight differences,
+            # we uniformize then by applying a median filter
+            filt_col = median_filter(self.offsets[..., 0], size=self.patch_size)
+            filt_lin = median_filter(self.offsets[..., 1], size=self.patch_size)
+            self.offsets = np.stack([filt_col, filt_lin], axis=-1)
 
         h_pad, w_pad = self.patchmatch.img_1_padded.shape[:2]
         grid = np.moveaxis(np.indices((h, w)), 0, -1)
@@ -51,6 +60,7 @@ class CopyPasteDetector:
         offset_magnitude = np.sqrt(self.offsets[..., 0]**2 + self.offsets[..., 1]**2)
         diffs = self.compute_differences()
         copy_paste_candidates = (diffs < self.diff_threshold) & (offset_magnitude >= self.min_norm)
+        copy_paste_candidates = binary_opening(copy_paste_candidates, structure=np.ones((5, 5)))
 
         valid_indices = []
         valid_offsets = []
@@ -59,7 +69,7 @@ class CopyPasteDetector:
         # use standard deviation to discard regions that are too flat
         for lin, col in zip(lin_coords, col_coords):
             patch = self.img[lin:lin + self.patch_size, col:col + self.patch_size]
-            if np.std(patch) > flat_threshold:
+            if np.std(patch) >= flat_threshold:
                 valid_indices.append((lin, col))
                 valid_offsets.append(self.offsets[lin, col])
 
@@ -68,50 +78,66 @@ class CopyPasteDetector:
         valid_offsets = np.array(valid_offsets)
         valid_indices = np.array(valid_indices)
 
-        # cluster offsets that are acceptably similar (differ at most [eps] from one another),
-        # as in a noisy image we may have some variance between offsets regarding the same copied-pasted region
-        clustering = DBSCAN(eps=cluster_eps, min_samples=min_cluster_size).fit(valid_offsets)
-        labels = clustering.labels_
-
-        # -1 means "noise"
-        unique_labels = set(labels)
-        if -1 in unique_labels:
-            unique_labels.remove(-1)
-        if len(unique_labels) == 0:
-            return np.zeros((h, w, 3), dtype=np.float32)
-
         output_visualization = np.zeros((h, w, 3), dtype=np.float32)
 
-        colors = hsv2rgb(np.array(
-            [[[i/len(unique_labels), 1.0, 1.0] for i in range(len(unique_labels))]]
-        ))[0]
+        if clustering:
+            # cluster offsets that are acceptably similar (differ at most [eps] from one another),
+            # as in a noisy image we may have some variance between offsets regarding the same copied-pasted region
+            clustering = DBSCAN(eps=cluster_eps, min_samples=min_cluster_size).fit(valid_offsets)
+            labels = clustering.labels_
 
-        for idx, label_id in enumerate(unique_labels):
-            current_color = colors[idx]
+            # -1 means "noise"
+            unique_labels = set(labels)
+            if -1 in unique_labels:
+                unique_labels.remove(-1)
+            if len(unique_labels) == 0:
+                return np.zeros((h, w, 3), dtype=np.float32)
 
-            mask_indices = (labels == label_id)
-            cluster_pixels = valid_indices[mask_indices]
-            cluster_offsets = valid_offsets[mask_indices]
+            output_visualization = np.zeros((h, w, 3), dtype=np.float32)
 
-            object_mask = np.zeros((h, w), dtype=bool)
-            for (plin, pcol) in cluster_pixels:
-                object_mask[plin, pcol] = True
+            colors = hsv2rgb(np.array(
+                [[[i/len(unique_labels), 1.0, 1.0] for i in range(len(unique_labels))]]
+            ))[0]
 
-            # morphological closing to try to fill the gaps inside a detected region,
-            # which may contain discontinuities (specially in a noisy image)
-            object_mask = binary_closing(object_mask, structure=np.ones((self.patch_size, self.patch_size)))
+            for idx, label_id in enumerate(unique_labels):
+                current_color = colors[idx]
 
-            filled_lin, filled_col = np.where(object_mask)
+                mask_indices = (labels == label_id)
+                cluster_pixels = valid_indices[mask_indices]
+                cluster_offsets = valid_offsets[mask_indices]
 
-            # set offset of filled pixels as the median of the cluster
-            avg_offset = np.median(cluster_offsets, axis=0).astype(int)
-            dlin, dcol = avg_offset
+                object_mask = np.zeros((h, w), dtype=bool)
+                for (plin, pcol) in cluster_pixels:
+                    object_mask[plin, pcol] = True
 
-            for plin, pcol in zip(filled_lin, filled_col):
-                output_visualization[plin, pcol] = current_color
-                dest_lin, dest_col = int(plin + dlin), int(pcol + dcol)
+                # morphological closing to try to fill the gaps inside a detected region,
+                # which may contain discontinuities (specially in a noisy image)
+                object_mask = binary_closing(object_mask, structure=np.ones((self.patch_size, self.patch_size)))
+
+                filled_lin, filled_col = np.where(object_mask)
+
+                # set offset of filled pixels as the median of the cluster
+                avg_offset = np.median(cluster_offsets, axis=0).astype(int)
+                dlin, dcol = avg_offset
+
+                for plin, pcol in zip(filled_lin, filled_col):
+                    output_visualization[plin, pcol] = current_color
+                    dest_lin, dest_col = int(plin + dlin), int(pcol + dcol)
+                    if 0 <= dest_lin < h and 0 <= dest_col < w:
+                        output_visualization[dest_lin, dest_col] = current_color
+        else:
+            # color by angle instead of by ID
+            angles = np.arctan2(valid_offsets[..., 1], valid_offsets[..., 0])
+
+            hues = (angles + np.pi) / (2 * np.pi)
+
+            for i, (lin, col) in enumerate(valid_indices):
+                rgb_color = hsv2rgb(np.array([[[hues[i], 1.0, 1.0]]]))[0][0]
+                output_visualization[lin, col] = rgb_color
+
+                dest_lin, dest_col = lin + int(valid_offsets[i][0]), col + int(valid_offsets[i][1])
                 if 0 <= dest_lin < h and 0 <= dest_col < w:
-                    output_visualization[dest_lin, dest_col] = current_color
+                    output_visualization[dest_lin, dest_col] = rgb_color
 
         return output_visualization
 
@@ -143,12 +169,13 @@ class CopyPasteDetector:
             for j in range(w):
                 patch_1 = get_patch(img_padded, i, j, self.patch_size)
                 patch_2 = get_patch(img_padded, i + self.offsets[i, j][0], j + self.offsets[i, j][1], self.patch_size)
-                diffs[i, j] = diff(patch_1, patch_2)
+                diffs[i, j] = diff(patch_1, patch_2, np.inf)
         return diffs
     
-    def detect_copy_paste(self):
+    def detect_copy_paste(self, min_norm=10):
+        offsets_magnitude = np.sqrt(self.offsets[..., 0]**2 + self.offsets[..., 1]**2)
         diffs = self.compute_differences()
-        mask = diffs < self.diff_threshold
+        mask = (diffs < self.diff_threshold) & (offsets_magnitude >= self.min_norm)
         return mask
 
     def detect_source_and_copy(self):
