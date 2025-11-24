7caa42b062a3b1ee6d255cca56adaf871b0cf563
Francisco Henriques <franciscolealhenriques@usp.br>
2025-11-24 02:50:22 +0100

    Add final experiment images and results

--- experiments/copy-paste.py
+++ experiments/copy-paste.py
@@ -17,22 +17,23 @@ from main.detector import CopyPasteDetector as CPD
 from utils.helpers import detection_results, display_results
 
 def detection_normal_image():
-    img_path = "../images/fleur_copy_paste_normal.png"
+    img_path = "../images/lena_inpainted_4.png"
 
     patch_size = 7
     iterations = 5
-    min_norm = 15
-    diff_threshold = 500
+    min_norm = 5
+    diff_threshold = 1500
     img = skio.imread(img_path)
+    img_orig = img.copy()
 
     detector = CPD(img, patch_size, iterations, min_norm, diff_threshold)
-    output_visualization = detector.visualize_offsets()
-    display_results(img_path, output_visualization, diff_threshold)
+    # output_visualization = detector.visualize_offsets()
+    # display_results(img, output_visualization)
 
     paired_result = detector.detect_paired_regions(
-        cluster_eps=2.0,
-        flat_threshold=0.05,
-        min_cluster_size=300
+        cluster_eps=3.0,
+        flat_threshold=0.01,
+        min_cluster_size=100
     )
     paired_result = (paired_result * 255).astype(np.uint8)
 
@@ -49,70 +50,41 @@ def detection_normal_image():
     overlay[mask] = paired_result[mask]
     # source_mask, copy_mask = detector.detect_source_and_copy()
     # result = detection_results(img_path, source_mask, copy_mask)
-    display_results(img_path, overlay, diff_threshold)
+    # skio.imsave("fleur_with_flat_threshold.png", overlay)
+    display_results(img_orig, overlay)
 
 
 def detection_noisy_image():
-    img_path = "../images/fleur_copy_paste_gauss.png"
-
+    img_path = "../images/lena_inpainted_4_impulse.png"
+    img = skio.imread(img_path)
     patch_size = 7
     iterations = 5
-    min_norm = 15
-    diff_threshold = 300
-    img = skio.imread(img_path)
+    min_norm = 5
+    diff_threshold = 100.0
+    img_orig = img.copy()
 
-    # Gaussian filter
-    img = gaussian_filter(img, sigma=1.5)
-
-    # NLM filter
-    # sigma_est = np.mean(estimate_sigma(img, channel_axis=-1))
-    # img = denoise_nl_means(
-    #     img,
-    #     h=.1,
-    #     fast_mode=True,
-    #     patch_size=5,
-    #     patch_distance=11,
-    #     channel_axis=-1
-    # )
-    # if img.dtype != np.uint8:
-    #     img = (np.clip(img, 0, 1) * 255).astype(np.uint8)
-
-    # Median filter
-    #if img.ndim == 3:
-    #    if img.shape[2] == 4:
-    #        img = img[..., :3]
-    #    elif img.shape[2] == 2:
-    #        img = img[..., 0]
-    #if img.ndim == 3:
-    #    for i in range(img.shape[2]):
-    #        img[:, :, i] = median_filter(img[:, :, i], size=3)
-    #else:
-    #    img = median_filter(img, size=5)
-
-    detector = CPD(img, patch_size, iterations, min_norm, diff_threshold)
-    output_visualization = detector.visualize_offsets()
-    display_results(img_path, output_visualization, diff_threshold)
-
-    paired_result = detector.detect_paired_regions(
-        cluster_eps=2.0,
-        flat_threshold=0.1,
-        min_cluster_size=300
-    )
-    paired_result = (paired_result * 255).astype(np.uint8)
-
+    detector = CPD(img, patch_size, iterations, min_norm, diff_threshold, median_filtering=True)
+    img = detector.img
     if img.ndim == 2:
         img = gray2rgb(img)
-    if img.ndim == 3:
-        if img.shape[2] == 4:
-            img = img[..., :3]
-        elif img.shape[2] == 2:
-            img = gray2rgb(img[..., 0])
-    mask = np.any(paired_result > 0, axis = -1)
-    overlay = img.copy()
-    overlay[mask] = paired_result[mask]
-    # source_mask, copy_mask = detector.detect_source_and_copy()
-    # result = detection_results(img_path, source_mask, copy_mask)
-    display_results(img_path, overlay, diff_threshold)
+    elif img.shape[2] == 4:
+        img = img[..., :3]
+    cluster_eps=1.0
+    flat_threshold=0.0
+    min_cluster_size=10
+    paired_result = detector.detect_paired_regions(
+        cluster_eps,
+        flat_threshold,
+        clustering=True,
+        min_cluster_size=min_cluster_size,
+        median_filtering=True,
+        median_filter_ksize=3,
+        spatial_weight=0.1
+    )
+    result = detection_results(img, paired_result)
+    display_results(img_orig, result)
+    # skio.imsave("suburbs_with_clustering.png", result)
 
 
+# detection_normal_image()
 detection_noisy_image()
